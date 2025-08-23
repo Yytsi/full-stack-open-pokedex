3a000466e1b8ab0cbb15541a4cf77e82b7093873
Yytsi <tuukka.yildirim@gmail.com>
2025-08-23 21:01:00 +0300

    11.19 works and now healthcheck is removed

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5001 // 5000 can be occupied on Mac
 
-app.get('/nothealthy', (req, res) => {
-  res.status(500).send('NOT ok!')
-})
+// app.get('/nothealthy', (req, res) => {
+//   res.status(500).send('NOT ok! this works and is tested now')
+// })
 
 app.get('/health', (req, res) => {
   res.send('ok')
