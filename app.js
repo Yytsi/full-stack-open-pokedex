cdf6e04842862145a2fab419d62cac4e7e176646
Yytsi <tuukka.yildirim@gmail.com>
2025-08-23 19:20:06 +0300

    11.19 trying to break soon

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5001 // 5000 can be occupied on Mac
 
+app.get('/break-prod', (req, res) => {
+  global.shouldFailHealthCheck = true
+  res.send('Got it, breaking prod :)')
+})
+
 app.get('/health', (req, res) => {
-  res.send('ok')
+  if (global.shouldFailHealthCheck) {
+    res.status(500).send('ok')
+  } else {
+    res.send('ok')
+  }
 })
 
 app.get('/version', (req, res) => {
