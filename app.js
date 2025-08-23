6fbd0ca7e2a24cad598e1115ccfb18a588152920
Yytsi <tuukka.yildirim@gmail.com>
2025-08-23 20:02:51 +0300

    11.19 need a different health checkgs

--- app.js
+++ app.js
@@ -4,17 +4,12 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5001 // 5000 can be occupied on Mac
 
-app.get('/break-prod', (req, res) => {
-  global.shouldFailHealthCheck = true
-  res.send('Got it, breaking prod :)')
+app.get('/not-healthy', (req, res) => {
+  res.status(500).send('NOT ok!')
 })
 
 app.get('/health', (req, res) => {
-  if (global.shouldFailHealthCheck) {
-    res.status(500).send('ok')
-  } else {
-    res.send('ok')
-  }
+  res.send('ok')
 })
 
 app.get('/version', (req, res) => {
