a0355856ab6928df9a0d01b48675f40b821b654f
Yytsi <tuukka.yildirim@gmail.com>
2025-08-23 20:03:05 +0300

    11.19 try2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5001 // 5000 can be occupied on Mac
 
-app.get('/not-healthy', (req, res) => {
+app.get('/nothealthy', (req, res) => {
   res.status(500).send('NOT ok!')
 })
 
